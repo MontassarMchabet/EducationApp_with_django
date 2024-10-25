8b32730e57a9a088e064959945809e42e1bed35c
Ons Khiari <98515359+ons-khiari@users.noreply.github.com>
2024-10-25 16:58:54 +0100

    Second Commit

--- Page/views.py
+++ Page/views.py
@@ -6,6 +6,21 @@ def page_list(request):
     pages = Page.objects.all()
     return render(request, 'page/page_list.html', {'pages': pages})
 
+def page_detail(request, chapter_id, page_id):
+    page = get_object_or_404(Page, id=page_id)
+
+    # Retrieve previous and next pages
+    previous_page = Page.objects.filter(id__lt=page_id, chapter_id=chapter_id).order_by('-id').first()
+    next_page = Page.objects.filter(id__gt=page_id, chapter_id=chapter_id).order_by('id').first()
+
+    context = {
+        'page': page,
+        'previous_page': previous_page,
+        'next_page': next_page,
+        'chapter_id': chapter_id,  # Pass chapter_id to the template for URL construction
+    }
+    return render(request, 'page/page_detail.html', context)
+
 def create_page(request):
     if request.method == "POST":
         form = PageForm(request.POST, request.FILES)  # Use request.FILES for file uploads
